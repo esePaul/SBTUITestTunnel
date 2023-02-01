b5a0953e4845ea1a035a2f35657f4be64ddd04b7
Marco Pagliari <lechuckcaptain@gmail.com>
2023-02-01 17:30:09 +0100

    wip

---
+++ Example/SBTUITestTunnel_Example_SwiftUI/SBTUITestTunnel_Example_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SBTUITestTunnel_Example_SwiftUIApp.swift
+//  SBTUITestTunnel_Example_SwiftUI
+//
+//  Created by Marco Pagliari on 01/02/23.
+//
+
+import SwiftUI
+
+@main
+struct SBTUITestTunnel_Example_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
